f5a0f060d5e328abb1b1c0dd7278c105833a08ff
Sambhawi Raj Nandini <119286849+Sambhawii@users.noreply.github.com>
2023-02-07 13:02:18 +0530

    Update Subsequence.java

--- Subsequence.java
+++ Subsequence.java
@@ -1,3 +1,5 @@
+
+
 import java.util.ArrayList;
 
 //get all the subsequence of a string and store in arraylist and return it
@@ -30,33 +32,4 @@ class subsequence {
     }
 }
 
-//ALTERNATIVE METHORD
 
-public class StringPermutations {
-
-    public static void main(String args[]) {
-        permutation("123");
-    }
-
-   public static void permutation(String input){
-          permutation("", input);
-   }
-
-   /*
-    * Recursive method which actually prints all permutations
-    * of given String, but since we are passing an empty String
-    * as current permutation to start with 
-    */
-   public static void permutation(String perm, String word) {
-        if (word.isEmpty()) {
-            System.err.println(perm + word);
-
-        } else {
-            for (int i = 0; i &lt; word.length(); i++) {
-                permutation(perm + word.charAt(i), word.substring(0, i) 
-                                + word.substring(i + 1, word.length()));
-            }
-        }
-
-    }
-}
