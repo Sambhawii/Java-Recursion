6fd843e8a71cc975ae5760c367e68db3c0b13ae5
Sambhawi Raj Nandini <119286849+Sambhawii@users.noreply.github.com>
2023-02-07 12:59:44 +0530

    Update PowerOfNum.java

--- PowerOfNum.java
+++ PowerOfNum.java
@@ -1,3 +1,26 @@
+//with recursion(without user input)
+
+public class PowerOfNum {
+    static boolean checkpower(int i, int n) {
+        // termination condition
+        if (i == 1) {
+            return true;
+        }
+        // main logic
+        if (i % n == 0) {
+            checkpower(i / n, n);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+    public static void main(String[] args) {
+        System.out.println(checkpower(4, 2));
+       }
+}
+
+//WITH USER INPUT
 import java.util.Scanner;
 
 public class PowerOfNum {
